25bf4f5226f7e6cce68e2e23e14660358de2629f
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-09-26 13:53:22 +0100

    mobile navigation

--- src/components/Plants.tsx
+++ src/components/Plants.tsx
@@ -6,14 +6,14 @@ const Plants: React.FC = () => {
   return (
     <>
       <div className="mt-12 px-8 font-Lora_Reg text-sm ">
-        <h3 className="px-4 py-2 font-Belleza font-extrabold text-base">
+        <h3 className=" md:px-4 py-2 font-Belleza font-extrabold text-base">
           OUR SHOP
         </h3>
-        <p className="px-4 py-2 font-Quicksand_L  text-base mb-4">
+        <p className="md:px-4 py-2 font-Quicksand_L  text-base mb-4">
           Here are the best seller from our varieties
         </p>
         <a
-          className="rounded-xl text-xs bg-black w-full h-fit text-white px-8 py-2 lg:w-fit lg:text-sm lg:rounded-2xl ml-3 "
+          className="rounded-xl text-lg bg-black w-full h-fit text-white px-8 py-2 lg:w-fit lg:text-sm lg:rounded-2xl md:ml-3 font-Quicksand_L"
           href="/shop"
         >
           See all
