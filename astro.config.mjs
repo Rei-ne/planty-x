8cd7a9465a6504a240b0f3b8977f81e12313e535
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-09-28 21:23:40 +0100

    trying with netlify functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone",
-  }),
-
+  adapter: netlify()
 });
---
+++ netlify/functions/contact.cjs
@@ -0,0 +1,26 @@
+exports.handler = async (event) => {
+  const request = event.request;
+  const data = await request.formData();
+  const name = data.get("name");
+  const email = data.get("email");
+  const message = data.get("message");
+
+  // Validate the data
+  if (!name || !email || !message) {
+    return new Response(
+      JSON.stringify({
+        message: "Missing required fields",
+      }),
+      { status: 400 }
+    );
+  }
+
+  const response = {
+    successMessage: "Success!",
+    name,
+    email,
+    message,
+  };
+
+  return new Response(JSON.stringify(response), { status: 200 });
+};
