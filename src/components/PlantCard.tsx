2e17fceb31f545fdb2971388f3d2a44a81bc453b
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2023-10-11 20:28:29 +0100

    ui updates - 2

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -15,11 +15,11 @@ const PlantCard: React.FC<Props> = ({ title, img, price }) => {
           src={img}
           alt="plant-image"
         />
-        <div className="mt-6 flex justify-between text-sm w-60 md:w-60 text-center">
-          <p className="w-1/2 px-2 flex items-start  font-Quicksand_B text-sm">
+        <div className="mt-6 flex justify-between text-sm w-40  sm:w-60    md:w-60 text-center">
+          <p className="w-1/2 px-2 flex items-start  font-Quicksand_B text-xs sm:text-sm">
             {title}
           </p>
-          <p className="w-1/2 flex justify-end px-2 font-Quicksand_L text-sm">
+          <p className="w-1/2 flex justify-end px-2 font-Quicksand_L text-xs sm:text-sm">
             {price}
           </p>
         </div>
